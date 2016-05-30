cf45624e6646567e2ef590d4670c139f588d37d3
Czentye Bence Levente <czentye.bence.levente@webtown.hu>
2016-05-30 13:45:50 +0200

    TvTracker portlets' skeletons

---
+++ docroot/WEB-INF/src/hu/webtown/liferay/portlet/tvtracker/tvtadmin/TvTrackerTvShowAdminPortlet.java
@@ -0,0 +1,11 @@
+package hu.webtown.liferay.portlet.tvtracker.tvtadmin;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class TvTrackerTvShowAdminPortlet
+ */
+public class TvTrackerTvShowAdminPortlet extends MVCPortlet {
+ 
+
+}
---
+++ docroot/WEB-INF/src/hu/webtown/liferay/portlet/tvtracker/tvtpublic/TvTrackerPublicPortlet.java
@@ -0,0 +1,11 @@
+package hu.webtown.liferay.portlet.tvtracker.tvtpublic;
+
+import com.liferay.util.bridges.mvc.MVCPortlet;
+
+/**
+ * Portlet implementation class TvTrackerPublicPortlet
+ */
+public class TvTrackerPublicPortlet extends MVCPortlet {
+ 
+
+}
